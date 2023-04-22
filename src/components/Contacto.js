90b0dc9c8546a165e973ffa75c05e84f67fa3729
marcosfacchetti <marcos-facchetti@hotmail.com>
2023-04-22 19:37:10 -0300

    dos

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {AiOutlineMail, AiOutlinePhone} from 'react-icons/ai'
 
 function Contacto() {
   return (
@@ -13,9 +14,11 @@ function Contacto() {
       justify-center 
     '>
       <p className='pb-10'>Contacto</p>
-      <div>
-      <p className="pb-5">Email: <a href="mailto:Marcos-facchetti@hotmail.com" className="text-blue-500 hover:text-blue-700">Marcos-facchetti@hotmail.com</a></p>
-      <p className="">Teléfono: <a href="tel:+543413025312" className="text-blue-500 hover:text-blue-700">+54 341 3025312</a></p>
+      <div className='flex'>
+      <AiOutlineMail/><p className="pb-5 ml-5">Email: <a href="mailto:Marcos-facchetti@hotmail.com" className="text-blue-500 hover:text-blue-700">Marcos-facchetti@hotmail.com</a></p>
+      </div>
+      <div className='flex'>
+      <AiOutlinePhone/><p className="pb-5 ml-5">Phone: <a href="tel:+543413025312" className="text-blue-500 hover:text-blue-700">+54 341 3025312</a></p>
       </div>
     </div>
   )
--- src/components/Habilidades.js
+++ src/components/Habilidades.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {IoLogoHtml5, IoLogoJavascript, IoLogoCss3, IoLogoPython} from 'react-icons/io'
-import {SiVisualstudio, SiPostgresql} from 'react-icons/si'
-import {FaBootstrap} from 'react-icons/fa'
+import {SiVisualstudio, SiPostgresql, SiTailwindcss} from 'react-icons/si'
+import {FaBootstrap, FaReact} from 'react-icons/fa'
 import {DiDjango} from 'react-icons/di'
 
 
@@ -18,16 +18,18 @@ function Habilidades() {
       justify-center 
     '>
         <p className='pb-10'>Habilidades</p>
-        <div className='grid grid-cols-2 gap-4 place-content-evenly'>
+        <div className='grid grid-cols-5 gap-4 place-content-evenly '>
 
-        <IoLogoHtml5 size="5rem" />
-        <IoLogoCss3 size="5rem" />
-        <IoLogoJavascript size="5rem" />
-        <IoLogoPython size="5rem" />
-        <SiVisualstudio size="5rem" />
-        <FaBootstrap size="5rem" />
-        <DiDjango size="5rem" />
-        <SiPostgresql size="5rem" />
+        <div className='flex flex-col text-center'><IoLogoHtml5 size="5rem" /><p>HTML</p></div>
+        <div className='flex flex-col text-center'><IoLogoCss3 size="5rem" /><p>CSS</p></div>
+        <div className='flex flex-col text-center'><IoLogoJavascript size="5rem" /><p>JavaScript</p></div>
+        <div className='flex flex-col text-center'><IoLogoPython size="5rem" /><p>Python</p></div>
+        <div className='flex flex-col text-center'><SiVisualstudio size="5rem" /><p>VSCode</p></div>
+        <div className='flex flex-col text-center'><FaBootstrap size="5rem" /><p>Bootstrap</p></div>
+        <div className='flex flex-col text-center'><DiDjango size="5rem" /></div>
+        <div className='flex flex-col text-center'><FaReact size="5rem" /><p>React</p></div>
+        <div className='flex flex-col text-center'><SiTailwindcss size="5rem" /><p>Tailwind</p></div>
+        <div className='flex flex-col text-center'><SiPostgresql size="5rem" /><p>Postgresql</p></div>
 
 
         </div>
