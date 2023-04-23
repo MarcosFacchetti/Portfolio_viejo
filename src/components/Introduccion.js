028e91e6604bbbeacf8069d94e936a1897237f8b
marcosfacchetti <marcos-facchetti@hotmail.com>
2023-04-23 16:55:35 -0300

    veinticinco

--- src/components/Introduccion.js
+++ src/components/Introduccion.js
@@ -11,7 +11,8 @@ function Introduccion() {
   }
 
   return (
-    <div className='
+    <div
+      className="
       min-h-screen
       flex 
       flex-col 
@@ -21,8 +22,10 @@ function Introduccion() {
       space-y-5 
       px-4
       md:px-0
-    '>
-      <div className='
+    "
+    >
+      <div
+        className="
         relative
         rounded-full
         overflow-hidden
@@ -34,29 +37,44 @@ function Introduccion() {
         hover:rotate-180 
         active:rotate-360 
         active:duration-2000
-      '
-      onClick={handleRotation}>
-        <img src={perfil} alt='Perfil' className={`w-full h-full object-cover ${rotating ? 'active' : ''}`}/>
+      "
+        onClick={handleRotation}
+      >
+        <img
+          src={perfil}
+          alt="Perfil"
+          className={`w-full h-full object-cover ${rotating ? "active" : ""}`}
+        />
       </div>
 
-        <div className="pointer-events-none">
-        <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Press+Start+2P&size=22&duration=2000&pause=1000&color=0E6EF7&vCenter=true&repeat=false&width=435&lines=Marcos+Facchetti" alt="Typing SVG" /></a>
-        </div>
+      <div className="pointer-events-none">
+        <a href="https://git.io/typing-svg">
+          <img
+            src="https://readme-typing-svg.demolab.com?font=Press+Start+2P&size=22&duration=2000&pause=1000&color=0E6EF7&vCenter=true&repeat=false&width=435&lines=Marcos+Facchetti"
+            alt="Typing SVG"
+          />
+        </a>
+      </div>
 
-        <div className="pointer-events-none">
-        <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Press+Start+2P&size=17&duration=1500&pause=1000&color=2CF73E&width=435&lines=Programador+Frontend+.+.+.;Programador+Backend+.+.+." alt="Typing SVG" /></a>
-        </div>
+      <div className="pointer-events-none">
+        <a href="https://git.io/typing-svg">
+          <img
+            src="https://readme-typing-svg.demolab.com?font=Press+Start+2P&size=17&duration=1500&pause=1000&color=2CF73E&width=435&lines=Programador+Frontend+.+.+.;Programador+Backend+.+.+."
+            alt="Typing SVG"
+          />
+        </a>
+      </div>
 
-        <div className="flex justify-center mt-8">
-      <a
-        href={resumePDF}
-        download="mi_curriculum.pdf"
-        className="bg-slate-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      <div className="flex justify-center mt-8">
+        <a
+          href={resumePDF}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-slate-950 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-        Descargar CV
-      </a>
-    </div>
-
+          - Ver Curriculum 🎓 -
+        </a>
+      </div>
     </div>
   );
 }
