e47a1c0db2dc873ff23cca69d43742d9bbd29ccc
marcosfacchetti <marcos-facchetti@hotmail.com>
2023-04-22 23:57:33 -0300

    catorce

--- src/components/Habilidades.js
+++ src/components/Habilidades.js
@@ -20,7 +20,7 @@ function Habilidades() {
     "
     >
       <p className="pb-10">Skills</p>
-      <div className="grid grid-cols-2 gap-x-20 gap-y-5 pl-10 lg:pl-10">
+      <div className="grid grid-cols-2 gap-x-20 gap-y-5 pl-0 lg:pl-10">
         <div className="flex flex-col items-center text-center sm:w-1/2">
           <IoLogoHtml5 size="5rem" />
           <p className="text-xs">HTML</p>
