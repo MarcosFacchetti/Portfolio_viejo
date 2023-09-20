d6472f3d36f9c04b89a434da7bc59ddd0ef1383f
marcosfacchetti <marcos-facchetti@hotmail.com>
2023-09-20 12:25:16 -0300

    cambio3

--- src/components/Introduccion.js
+++ src/components/Introduccion.js
@@ -50,7 +50,7 @@ function Introduccion() {
             isDarkMode ? "bg-[#ffffff3b]" : "bg-gray-600 text-white"
           } mb-2 font-Montserrat-LightItalic py-2 px-4 rounded`}
         >
-          ->  Toca para ver Currículum 🎓  <-
+          -  Toca para ver Currículum 🎓  -
         </a>
       </div>
       <div>
